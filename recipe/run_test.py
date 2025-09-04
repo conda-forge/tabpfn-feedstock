1b4a53450f99ee464c71a272021a10ac895e56b3
Jan Jagusch <jan.jagusch@gmail.com>
2025-09-04 10:56:22 +0200

    Add reference to issue.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,6 +1,7 @@
 import os
 
 # Importing tabpfn fails when 'CI' environment variable is set to anything else than 'true' or 'false' (e.g., 'azure').
+# Ref.: https://github.com/PriorLabs/TabPFN/issues/482
 os.environ["CI"] = "true"
 
 import tabpfn
